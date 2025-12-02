26a88d381433206d06667bf1f08b360208e74f5a
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-02 16:57:49 +0200

    header

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -1,10 +1,24 @@
+import { Link } from "react-router"
+import { navLinks } from "../../constants/navLinks"
 
 const Header = () => {
     return (
-        <header className="container py-3">
+        <header className="container py-3 flex items-center justify-center gap-[114px]">
             <img
                 src="/images/header/numra_logo.png"
             />
+
+            <div className="flex items-center gap-6">
+                {navLinks.map((link) => (
+                    <Link
+                        key={link.title}
+                        to={link.href}
+                        className="text-[#192540] hover-text-[#EBAF29]"
+                    >
+                        {link.title}
+                    </Link>
+                ))}
+            </div>
         </header>
     )
 }
---
+++ src/constants/navLinks.ts
@@ -0,0 +1,23 @@
+
+export const navLinks =[
+    {
+        title: "Home",
+        href: "/"
+    },
+    {
+        title: "Draw your plate",
+        href: "/"
+    },
+    {
+        title: "Sell your plate",
+        href: "/"
+    },
+    {
+        title: "FAQs",
+        href: "/"
+    },
+    {
+        title: "Contact Us",
+        href: "/"
+    },
+]
