096eb4cf005f4901feab84b30a67e08faaca20cb
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-03 13:21:59 +0200

    adds Why Choose Numra section

---
+++ src/components/home/HomeImageSection.tsx
@@ -0,0 +1,13 @@
+const HomeImageSection = () => {
+    return (
+        <div className="w-full">
+            <img
+                src="/images/home/home_image.png"
+                alt="numra"
+                className="w-full"
+            />
+        </div>
+    )
+}
+
+export default HomeImageSection
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import FilterYourPlates from "../components/home/FilterYourPlates"
 import HomeCategories from "../components/home/HomeCategories"
 import HomeHero from "../components/home/HomeHero"
+import HomeImageSection from "../components/home/HomeImageSection"
+import WhyChooseNumra from "../components/home/WhyChooseNumra"
 
 const HomePage = () => {
     return (
@@ -8,6 +10,8 @@ const HomePage = () => {
             <HomeHero />
             <HomeCategories />
             <FilterYourPlates />
+            <HomeImageSection />
+            <WhyChooseNumra />
         </div>
     )
 }
