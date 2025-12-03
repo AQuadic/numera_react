b591d5a1f4f12516cf303205b122fc89f7ddf7e1
ahmed <ahmedabozaid.dev@gmail.com>
2025-12-03 10:08:34 +0200

    fixes tailwind

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -1,38 +1,36 @@
-import { Link } from "react-router"
-import { navLinks } from "../../constants/navLinks"
-import Chat from "../icons/header/Chat"
-import Notifications from "../icons/header/Notifications"
-import Profile from "../icons/header/Profile"
+import { Link } from "react-router";
+import { navLinks } from "../../constants/navLinks";
+import Chat from "../icons/header/Chat";
+import Notifications from "../icons/header/Notifications";
+import Profile from "../icons/header/Profile";
 
 const Header = () => {
-    return (
-        <header className="container py-3 flex items-center justify-center gap-[114px]">
-            <img
-                src="/images/header/numra_logo.png"
-            />
+  return (
+    <header className="container py-3 flex items-center justify-center gap-[114px]">
+      <img src="/images/header/numra_logo.png" />
 
-            <div className="flex items-center gap-6">
-                {navLinks.map((link) => (
-                    <Link
-                        key={link.title}
-                        to={link.href}
-                        className="text-[#192540] hover-text-[#EBAF29]"
-                    >
-                        {link.title}
-                    </Link>
-                ))}
-            </div>
+      <div className="flex items-center gap-6">
+        {navLinks.map((link) => (
+          <Link
+            key={link.title}
+            to={link.href}
+            className="text-[#192540] hover-text-[#EBAF29]"
+          >
+            {link.title}
+          </Link>
+        ))}
+      </div>
 
-            <div className="flex items-center gap-6">
-                <Chat />
-                <Notifications />
-                <button className="bg-[#EBAF29] w-[180px] h-14 rounded-[20px] text-[#192540] text-lg font-semibold flex items-center justify-center gap-2 ">
-                    <Profile />
-                    Log In
-                </button>
-            </div>
-        </header>
-    )
-}
+      <div className="flex items-center gap-6">
+        <Chat />
+        <Notifications />
+        <button className="bg-[#EBAF29] w-[180px] h-14 rounded-[20px] text-[#192540] text-lg font-semibold flex items-center justify-center gap-2 ">
+          <Profile />
+          Log In
+        </button>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
