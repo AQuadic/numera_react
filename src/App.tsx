b4260bf0aa9e298b10abb8fffdc6c4579c0c7466
ahmed <ahmedabozaid.dev@gmail.com>
2025-12-03 10:56:21 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./components/general/Layout";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
