234241dbc66b12aecf3240f6e7f784fa3305fd5d
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-04 12:00:59 +0200

    adds contact to get the price dialog

--- src/components/phone_number_details/PhoneNumDetailsHeader.tsx
+++ src/components/phone_number_details/PhoneNumDetailsHeader.tsx
@@ -1,5 +1,9 @@
 import Heart from "../icons/home/Heart"
+import Call from "../icons/plates/Call"
+import DialogWhatsapp from "../icons/plates/DialogWhatsapp"
 import Share from "../icons/plates/Share"
+import Verified from "../icons/plates/Verified"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog"
 
 const PhoneNumDetailsHeader = () => {
     return (
@@ -42,9 +46,50 @@ const PhoneNumDetailsHeader = () => {
 
             </div>
 
-            <button className="w-full h-14 bg-[#EBAF29] rounded-md mt-6 text-[#192540] text-lg font-semibold">
-                Price on Request
-            </button>
+            <Dialog>
+                <DialogTrigger className="w-full">
+                    <button className="w-full h-14 bg-[#EBAF29] rounded-md mt-6 text-[#192540] text-lg font-semibold cursor-pointer">
+                        Price on Request
+                    </button>
+                </DialogTrigger>
+                <DialogContent className="w-[860px]">
+                    <DialogHeader>
+                    <DialogTitle className="text-[#192540] text-2xl font-medium">Contact to Get the Price</DialogTitle>
+                    <DialogDescription>
+                        <div>
+                            <div className="w-full h-[102px] bg-[#FDFAF3] rounded-md mt-4 py-3 md:px-6 px-2 flex justify-between">
+                                <div className="flex items-center gap-4">
+                                    <img 
+                                    src="/images/plates/owner_img.jpg"
+                                    alt="owner image"
+                                    className="w-[78px] h-[78px] rounded-full"
+                                    />
+                                    <div>
+                                        <h2 className="text-[#192540] md:text-2xl font-medium">Ahmed Mohamed</h2>
+                                        <p className="text-[#717171] md:text-base font-medium">Premium Dealer</p>
+                                        <p className="text-[#717171] md:text-base font-medium">Member since 2019</p>
+                                    </div>
+                                </div>
+
+                                <Verified />
+                            </div>
+
+                            <div className="mt-8">
+                                <div className="w-full h-14 bg-[#192540] rounded-[10px] flex items-center justify-center gap-2">
+                                    <Call />
+                                    <p className="text-[#FEFEFE] text-lg font-medium">Call</p>
+                                </div>
+
+                                <div className="w-full h-14 bg-[#19AA3D] rounded-[10px] flex items-center justify-center gap-2 mt-4">
+                                    <DialogWhatsapp />
+                                    <p className="text-[#FEFEFE] text-lg font-medium">Whatsapp</p>
+                                </div>
+                            </div>
+                        </div>
+                    </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
         </section>
     )
 }
