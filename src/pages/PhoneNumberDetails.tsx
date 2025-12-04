afb81a8f3ebb25fd4413326d73e9a2b1bfec9b12
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-04 11:44:20 +0200

    complate phone number details page

--- src/pages/PhoneNumberDetails.tsx
+++ src/pages/PhoneNumberDetails.tsx
@@ -1,9 +1,15 @@
+import WhyChooseNumra from "../components/home/WhyChooseNumra"
 import PhoneNumDetailsHeader from "../components/phone_number_details/PhoneNumDetailsHeader"
+import PurchaseInstructions from "../components/plate_details/PurchaseInstructions"
+import SafetyTips from "../components/plate_details/SafetyTips"
 
 const PhoneNumberDetails = () => {
     return (
         <div>
             <PhoneNumDetailsHeader />
+            <PurchaseInstructions />
+            <SafetyTips />
+            <WhyChooseNumra />
         </div>
     )
 }
