367285d028fc454bb933bd0182290f18affeb152
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-07 10:41:17 +0200

    adds my profile component

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Sidebar from "../components/my_profile/Sidebar";
+import MyProfileComponent from "../components/my_profile/MyProfileComponent";
+import WhyChooseNumra from "../components/home/WhyChooseNumra";
 
 const MyProfile = () => {
     const [selected, setSelected] = useState("profile");
@@ -7,7 +9,7 @@ const MyProfile = () => {
     const renderContent = () => {
         switch (selected) {
             case "profile":
-                return <div className="p-6">Profile Component</div>;
+                return <MyProfileComponent />
             case "ads":
                 return <div className="p-6">My Ads Component</div>;
             case "analytics":
@@ -24,12 +26,16 @@ const MyProfile = () => {
     };
 
     return (
-        <div className="flex gap-6 py-6">
-            <Sidebar selected={selected} onSelect={setSelected} />
-            <div className="flex-1 bg-white rounded-xl shadow p-6">
-                {renderContent()}
+        <section>
+            <div className="flex gap-6 py-6">
+                <Sidebar selected={selected} onSelect={setSelected} />
+                <div className="flex-1 py-12 container">
+                    {renderContent()}
+                </div>
             </div>
-        </div>
+
+            <WhyChooseNumra />
+        </section>
     );
 };
 
