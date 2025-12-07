7c54d3b29102aa87ea5f237f72a8f4ef501405f2
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-07 12:14:40 +0200

    adds pop over

--- src/components/my_profile/ProfilePlates.tsx
+++ src/components/my_profile/ProfilePlates.tsx
@@ -1,9 +1,14 @@
 import Chat from "../icons/profile/Chat"
+import Delete from "../icons/profile/Delete"
+import Edit from "../icons/profile/Edit"
 import Heart from "../icons/profile/Heart"
 import MeatballsMenu from "../icons/profile/MeatballsMenu"
 import PlatePaused from "../icons/profile/PlatePaused"
 import Republish from "../icons/profile/Republish"
+import Share from "../icons/profile/Share"
+import SoldActions from "../icons/profile/SoldActions"
 import Views from "../icons/profile/Views"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
 
 const ProfilePlates = () => {
     return (
@@ -14,7 +19,30 @@ const ProfilePlates = () => {
                         <div className="px-6 py-2 bg-[#B2E3C4] rounded-[20px] text-[#1E7634] font-medium">Active</div>
                         <p className="text-[#717171] text-[10px]">Expires in 25 days</p>
                     </div>
-                    <MeatballsMenu />
+
+                    <Popover>
+                        <PopoverTrigger className="cursor-pointer">
+                            <MeatballsMenu />
+                        </PopoverTrigger>
+                        <PopoverContent className="w-[155px]">
+                            <div className="flex items-center gap-2 cursor-pointer">
+                                <Edit />
+                                <p className="text-[#192540] text-lg font-medium">Edit</p>
+                            </div>
+                            <div className="flex items-center gap-2 cursor-pointer mt-4">
+                                <Share />
+                                <p className="text-[#192540] text-lg font-medium">Share</p>
+                            </div>
+                            <div className="flex items-center gap-2 cursor-pointer mt-4">
+                                <SoldActions />
+                                <p className="text-[#192540] text-lg font-medium">Sold</p>
+                            </div>
+                            <div className="flex items-center gap-2 cursor-pointer mt-4">
+                                <Delete />
+                                <p className="text-[#D71F1F] text-lg font-medium">Delete</p>
+                            </div>
+                        </PopoverContent>
+                    </Popover>
                 </div>
 
                 <div className="mt-3 flex gap-4">
