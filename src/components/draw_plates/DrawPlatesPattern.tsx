85456b57197f72495b47e050737d041e7e6228c8
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-08 11:56:51 +0200

    change draw plates design

--- src/components/draw_plates/DrawPlatesPattern.tsx
+++ src/components/draw_plates/DrawPlatesPattern.tsx
@@ -1,111 +1,123 @@
 import { useState } from "react";
-import DeleteIcon from "../icons/plates/DeleteIcon";
-import XIcon from "../icons/plates/XIcon";
-import { Link } from "react-router";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 
 const DrawPlatesPattern = () => {
-  const [pattern, setPattern] = useState<string>("");
-    const [selectedChars, setSelectedChars] = useState<string[]>([]); 
-
-    const letters = ["X", "Y", "Z"];
-    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
-
-    const addCharacter = (char: string) => {
-        setPattern((prev) => prev + char);
-        setSelectedChars((prev) =>
-        prev.includes(char) ? prev : [...prev, char]
-        );
-    };
-
-    const clearAll = () => {
-        setPattern("");
-        setSelectedChars([]);
-    };
-
-    const removeLast = () => {
-        setPattern((prev) => prev.slice(0, -1));
-        setSelectedChars((prev) => prev.slice(0, -1));
-    };
-
-    const isSelected = (char: string) => selectedChars.includes(char);
+    const [bgColor, setBgColor] = useState("black");
+    const [barColor, setBarColor] = useState("white");
+    const [details, setDetails] = useState("");
+    const [mobile, setMobile] = useState("");
 
     return (
         <section className="px-4">
-        <h2 className="text-[#192540] md:text-[32px] text-xl font-medium">
-            Choose Plate Number Pattern
-        </h2>
-        <p className="text-[#717171] md:text-base text-xs font-medium leading-[150%] mt-2">
-            Tap letters and numbers to build a custom sequence. You'll see results <br /> that match your exact pattern (e.g., XY1YX).
-        </p>
-
-        <div className="mt-6">
-            <h3 className="text-[#192540] text-xl font-medium">Your Pattern</h3>
-            <div className="mt-4 relative flex flex-wrap gap-6">
-            <input
-                type="text"
-                value={pattern}
-                readOnly
-                className="lg:w-[692px] w-full h-[72px] border border-[#F0F0F0] rounded-md px-2"
-                placeholder="Start building your pattern"
-            />
-            <div
-                className="absolute top-6 right-4 lg:right-20 xl:right-64 cursor-pointer"
-                onClick={removeLast}
-            >
-                <XIcon />
-            </div>
 
             <div
-                className="md:w-[215px] w-full h-[72px] border border-[#F0F0F0] rounded-md flex items-center justify-center gap-2 cursor-pointer"
-                onClick={clearAll}
+                className="w-96 h-72 border flex flex-col justify-end mx-auto mb-8"
+                style={{ backgroundColor: bgColor }}
             >
-                <DeleteIcon />
-                <p className="text-[#D71F1F] text-lg font-semibold">Clear All</p>
-            </div>
-            </div>
-        </div>
+                <h2 className="text-white text-lg font-medium mb-2 px-4">
+                    {details}
+                </h2>
 
-        <div className="mt-6">
-            <h3 className="text-[#192540] text-xl font-medium">Select Characters</h3>
-
-            <p className="text-[#717171] text-lg font-medium mt-6">Letters</p>
-            <div className="mt-3 flex gap-6">
-            {letters.map((letter) => (
                 <div
-                key={letter}
-                className={`md:w-[95px] md:h-[94px] w-20 h-20 rounded-md text-[#192540] text-5xl font-medium flex items-center justify-center cursor-pointer ${
-                    isSelected(letter) ? "bg-[#EBAF29]" : "bg-[#F0F0F0]"
-                }`}
-                onClick={() => addCharacter(letter)}
+                    className="w-full h-10 flex"
+                    style={{ backgroundColor: barColor }}
                 >
-                {letter}
+                    <p className="flex items-center px-4">
+                        To contact: {mobile}
+                    </p>
                 </div>
-            ))}
             </div>
-        </div>
 
-        <div className="mt-6">
-            <p className="text-[#717171] text-lg font-medium mt-6">Numbers</p>
-            <div className="mt-3 grid grid-cols-4 gap-6 md:w-[450px]">
-            {numbers.map((num) => (
-                <div
-                key={num}
-                className={`md:w-[95px] md:h-[94px] w-20 h-20 rounded-md text-[#192540] text-5xl font-medium flex items-center justify-center cursor-pointer ${
-                    isSelected(num) ? "bg-[#EBAF29]" : "bg-[#F0F0F0]"
-                }`}
-                onClick={() => addCharacter(num)}
-                >
-                {num}
+            <h2 className="text-[#192540] md:text-[32px] text-xl font-medium">
+                Choose Plate Number Pattern
+            </h2>
+
+            <div className="flex flex-wrap items-center gap-4 mt-4 w-full">
+                <Select>
+                    <SelectTrigger className="lg:w-96 w-full h-12">
+                        <SelectValue placeholder="Dubai" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="1">Dubai</SelectItem>
+                        <SelectItem value="2">Dubai</SelectItem>
+                        <SelectItem value="3">Dubai</SelectItem>
+                    </SelectContent>
+                </Select>
+
+                <input 
+                    type="text"
+                    className="lg:w-96 w-full h-12 border rounded-md px-4"
+                    placeholder="Number"
+                />
+
+                <input 
+                    type="text"
+                    className="lg:w-96 w-full h-12 border rounded-md px-4"
+                    placeholder="Price"
+                />
+            </div>
+
+            <div className="mt-4">
+                <label className="text-[#192540] text-base font-medium">
+                    Contact number
+                </label>
+                <input 
+                    type="text"
+                    className="w-full h-12 border rounded-md mt-2 px-4"
+                    placeholder="Mobile"
+                    value={mobile}
+                    onChange={(e) => setMobile(e.target.value)}
+                />
+            </div>
+
+            <div className="mt-4">
+                <label className="text-[#192540] text-base font-medium">
+                    Additional details
+                </label>
+                <input 
+                    type="text"
+                    className="w-full h-12 border rounded-md mt-2 px-4"
+                    placeholder="Comment"
+                    value={details}
+                    onChange={(e) => setDetails(e.target.value)}
+                />
+            </div>
+
+            <div className="mt-4">
+                <p className="text-[#192540] text-base font-medium">Background color</p>
+                <div className="mt-2 flex items-center gap-4">
+                    <div
+                        className="w-full h-12 border bg-white rounded-md cursor-pointer"
+                        onClick={() => setBgColor("white")}
+                    ></div>
+
+                    <div
+                        className="w-full h-12 border bg-black rounded-md cursor-pointer"
+                        onClick={() => setBgColor("black")}
+                    ></div>
                 </div>
-            ))}
             </div>
-        </div>
 
-        <Link to='/plates_result'>
-            <button className="md:w-[450px] w-full h-14 bg-[#EBAF29] rounded-md mt-6 text-[#192540] text-lg font-semibold cursor-pointer">
-                Search matching plates
-            </button>
-        </Link>
+            <div className="mt-4">
+                <p className="text-[#192540] text-base font-medium">Communication bar color</p>
+                <div className="mt-2 flex items-center gap-4">
+                    <div
+                        className="w-full h-12 border bg-[#E50000] rounded-md cursor-pointer"
+                        onClick={() => setBarColor("#E50000")}
+                    ></div>
+
+                    <div
+                        className="w-full h-12 border bg-white rounded-md cursor-pointer"
+                        onClick={() => setBarColor("white")}
+                    ></div>
+
+                    <div
+                        className="w-full h-12 border bg-[#373737] rounded-md cursor-pointer"
+                        onClick={() => setBarColor("#373737")}
+                    ></div>
+                </div>
+            </div>
+
         </section>
     );
 };
