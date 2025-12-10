d0f2f351c6f4ab5b03ca12733dbcdb010f45f39f
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-10 14:31:41 +0200

    fix width - why choose numra

--- src/components/home/WhyChooseNumra.tsx
+++ src/components/home/WhyChooseNumra.tsx
@@ -18,13 +18,13 @@ const WhyChooseNumra = () => {
 
             <div className="md:mt-20 mt-5 flex flex-wrap items-center justify-between gap-8">
                 {data?.choose_numera.map((item: ChooseNumraItem, index: number) => (
-                <div key={index} className="relative">
+                <div key={index} className="relative md:w-[385px] w-full">
                     <img src={`/images/home/${index + 1}.png`} className="md:block hidden absolute -left-16 top-0 text-[240px] font-bold text-[#F0F0F0] select-none -z-10"/>
                     <div className="bg-[#FDFAF3] rounded-full w-[86px] h-[86px] flex items-center justify-center">
                         <img src={item.icon} alt="icon" className="relative z-10 w-[50px] h-[50px] " />
                     </div>
-                    <h2 className="text-[#192540] md:text-2xl text-xl font-semibold mt-5">{item.Question.en}</h2>
-                    <p className="w-[315px] text-[#717171] text-sm font-medium mt-3">{item.Answer.en}</p>
+                    <h2 className="text-[#192540] md:text-2xl text-xl font-semibold mt-3">{item.Question.en}</h2>
+                    <p className="w-[315px] text-[#717171] text-sm font-medium mt-2">{item.Answer.en}</p>
                 </div>
             ))}
             </div>
