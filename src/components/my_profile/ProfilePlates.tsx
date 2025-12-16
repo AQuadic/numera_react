67d808cd52cd1bb0c8c33a9c74f12979f41c0c20
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-16 12:29:12 +0200

    pause ads dialog

--- src/components/my_profile/ProfilePlates.tsx
+++ src/components/my_profile/ProfilePlates.tsx
@@ -24,6 +24,7 @@ const ProfilePlates = ({ plate, refetch }: ProfilePlatesProps) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isRepublishing, setIsRepublishing] = useState(false);
   const [isPaused, setIsPaused] = useState(!plate.paused_at);
+  const [isPauseDialogOpen, setIsPauseDialogOpen] = useState(false);
 
   useEffect(() => {
     setIsPaused(!plate.paused_at);
@@ -40,6 +41,7 @@ const ProfilePlates = ({ plate, refetch }: ProfilePlatesProps) => {
         await pauseSim({ plate_id: plateId });
       }
       setIsPaused(!isPaused);
+      setIsPauseDialogOpen(false);
       refetch?.();
     } catch (error) {
       console.error(error);
@@ -55,7 +57,7 @@ const ProfilePlates = ({ plate, refetch }: ProfilePlatesProps) => {
         plate_id: plate.id,
         package_user_id: plate.package_user_id,
       });
-      
+
       if (response.success) {
         console.log(response.message || "Plate republished successfully");
         refetch?.();
@@ -69,149 +71,186 @@ const ProfilePlates = ({ plate, refetch }: ProfilePlatesProps) => {
 
   return (
     <section>
-      <div className="md:w-[348px] w-full bg-[#F0F0F0] rounded-md px-4 py-3 bg-[url('/images/plates/plate_stars.png')] bg-no-repeat bg-position-[center_-0px]">
-        <div className="flex flew items-center justify-between">
-          <div className="flex items-center gap-2">
-            <div className="px-6 py-2 bg-[#B2E3C4] rounded-[20px] text-[#1E7634] font-medium">
-              {plate.is_active
-                ? "Active"
-                : plate.is_sold
-                ? "Sold"
-                : plate.paused_at
-                ? "Paused"
-                : "Continue"} 
-            </div>
-            <p className="text-[#717171] text-[10px]">Expires in 25 days</p>
-          </div>
-
-          <Popover>
-            <PopoverTrigger className="cursor-pointer">
-              <MeatballsMenu />
-            </PopoverTrigger>
-            <PopoverContent className="w-[155px]">
-              <div className="flex items-center gap-2 cursor-pointer">
+    <div className="bg-white p-3 rounded-[20px] shadow-custom-sm flex flex-col gap-4">
+      <div className="w-full flex items-center justify-between gap-4">
+        <div className="bg-[#EBAF29] px-3 py-1 rounded-[5px]">
+          <p className="text-xs font-medium text-[#192540]">
+            {plate.is_active
+              ? "Active"
+              : plate.is_sold
+              ? "Sold"
+              : plate.paused_at
+              ? "Paused"
+              : "Continue"}
+          </p>
+        </div>
+        <p className="text-sm font-normal text-[#717171]">
+          Expires in 25 days
+        </p>
+        <Popover>
+          <PopoverTrigger>
+            <MeatballsMenu />
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0">
+            <div className="bg-white flex flex-col gap-4 px-6 py-5 rounded-md w-52">
+              <button className="flex items-center gap-2">
                 <Edit />
-                <p className="text-[#192540] text-lg font-medium">Edit</p>
-              </div>
-              <div className="flex items-center gap-2 cursor-pointer mt-4">
+                <p className="text-[#192540] text-base font-medium">Edit</p>
+              </button>
+              <button className="flex items-center gap-2">
                 <Share />
-                <p className="text-[#192540] text-lg font-medium">Share</p>
-              </div>
-              <div className="flex items-center gap-2 cursor-pointer mt-4">
+                <p className="text-[#192540] text-base font-medium">Share</p>
+              </button>
+              <button className="flex items-center gap-2">
                 <SoldActions />
-                <p className="text-[#192540] text-lg font-medium">Sold</p>
-              </div>
-              <div className="flex items-center gap-2 cursor-pointer mt-4">
+                <p className="text-[#192540] text-base font-medium">Sold</p>
+              </button>
+              <button className="flex items-center gap-2">
                 <Delete />
-                <p className="text-[#D71F1F] text-lg font-medium">Delete</p>
-              </div>
-            </PopoverContent>
-          </Popover>
-        </div>
-
-        <div className="mt-3 flex flex-wrap gap-4">
-          <img src={plate.image_url} alt="plate" />
-          <div>
-            <h2 className="text-[#192540] text-base font-medium">
-              {plate.price}{" "}
-              <span className="text-base relative top-1">AED</span>
-            </h2>
-
+                <p className="text-[#F53535] text-base font-medium">Delete</p>
+              </button>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+      <div className="w-full relative">
+       <img src={plate.image_url} alt="plate" />
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="flex items-start justify-between gap-4">
+          <div className="flex flex-col gap-2">
+            <p className="text-[#192540] text-2xl font-semibold">
+              {plate.price} <span className="text-base font-medium">AED</span>
+            </p>
             {plate.old_price && (
-              <h2 className="text-[#A3A3A3] text-sm font-medium">
-                {plate.old_price}{" "}
-                <span className="text-sm relative top-1">AED</span>
-              </h2>
+              <p className="text-[#8E8E93] text-xl font-medium line-through">
+                {plate.old_price} <span className="text-sm">AED</span>
+              </p>
             )}
-
-            {/* Negotiable tag removed from profile card */}
           </div>
         </div>
-
-        <div className="mt-2 relative">
-          <img src="/images/plates/available_img.png" alt="bg" />
-          {/* Removed 'Available' overlay text from profile card */}
-        </div>
-
-        <div className="mt-4 flex flex-wrap items-center gap-4">
-          <div className="flex items-center gap-2">
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-1">
             <Views />
-            <p className="text-[#717171] text-[10px] font-medium">
+            <p className="text-[#192540] text-base font-normal">
               {plate.views_count} Views
             </p>
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-1">
             <Heart />
-            <p className="text-[#717171] text-[10px] font-medium">
+            <p className="text-[#192540] text-base font-normal">
               {plate.favorites_count} Favorites
             </p>
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-1">
             <Chat />
-            <p className="text-[#717171] text-[10px] font-medium">
+            <p className="text-[#192540] text-base font-normal">
               {plate.chats_count} Chats
             </p>
           </div>
         </div>
-
-        <div className="flex items-center gap-3 mt-4">
-            <Dialog>
-              <DialogTrigger className="w-full">
-                <button
-                  
-                  className="xl:w-[152px] w-full h-11 rounded-[10px] bg-[#EBAF29] flex items-center justify-center gap-1 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
-                >
-                  <Republish />
-                  <p className="text-[#192540] text-base font-medium">
-                    Republish
-                  </p>
-                </button>
-              </DialogTrigger>
-              <DialogContent className="w-[860px] px-0!">
-                  <DialogHeader>
-                  <DialogTitle ></DialogTitle>
-                  <DialogDescription>
-                      <div>
-                        <div className="flex flex-col items-center justify-center">
-                          <img 
-                          src="../../../public/images/republish.png"
-                          alt="republish"  
-                        />
-                        <h2 className="text-[#192540] text-2xl font-semibold mt-4">Republish Your Ad ?</h2>
-                        <p className="text-[#717171] text-lg font-medium mt-4">Would you like to republish it to keep it active and visible to buyers?</p>
-                        </div>
-                        <div className="flex items-center justify-between gap-6 mt-7 px-8">
-                          <button
-                            className="w-full h-14 border border- [#EBAF29] rounded-md text-[#192540] text-lg font-semibold cursor-pointer"
-                          >
-                            Cancel  
-                          </button>
-                          <button
-                            onClick={handleRepublish}
-                            disabled={isRepublishing}
-                            className="w-full h-14 bg-[#EBAF29] rounded-md text-[#192540] text-lg font-semibold cursor-pointer"
-                          >
-                            {isRepublishing ? "Publishing..." : "Republish Now"}
-                          </button>
-                        </div>
-                      </div>
-                  </DialogDescription>
-                  </DialogHeader>
-              </DialogContent>
+        <div className="flex items-center justify-between gap-4">
+          <Dialog>
+            <DialogTrigger className="w-full">
+              <button className="xl:w-[152px] w-full h-11 rounded-[10px] bg-[#EBAF29] flex items-center justify-center gap-1 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer">
+                <Republish />
+                <p className="text-[#192540] text-base font-medium">
+                  Republish
+                </p>
+              </button>
+            </DialogTrigger>
+            <DialogContent className="w-[860px] px-0!">
+              <DialogHeader>
+                <DialogTitle></DialogTitle>
+                <DialogDescription>
+                  <div>
+                    <div className="flex flex-col items-center justify-center">
+                      <img
+                        src="../../../public/images/republish.png"
+                        alt="republish"
+                      />
+                      <h2 className="text-[#192540] text-2xl font-semibold mt-4">
+                        Republish Your Ad ?
+                      </h2>
+                      <p className="text-[#717171] text-lg font-medium mt-4">
+                        Would you like to republish it to keep it active and
+                        visible to buyers?
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between gap-6 mt-7 px-8">
+                      <button className="w-full h-14 border border-[#EBAF29] rounded-md text-[#192540] text-lg font-semibold cursor-pointer">
+                        Cancel
+                      </button>
+                      <button
+                        onClick={handleRepublish}
+                        disabled={isRepublishing}
+                        className="w-full h-14 bg-[#EBAF29] rounded-md text-[#192540] text-lg font-semibold cursor-pointer"
+                      >
+                        {isRepublishing ? "Publishing..." : "Republish Now"}
+                      </button>
+                    </div>
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
           </Dialog>
 
-          <button
-            onClick={() => handleTogglePause(plate.id)}
-            disabled={isLoading}
-            className="xl:w-[152px] w-full h-11 rounded-[10px] bg-[#E4E4E4] flex items-center justify-center gap-1 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
-          >
-            <PlatePaused />
-            <p className="text-[#192540] text-base font-medium">
-              {isLoading ? "Loading..." : isPaused ? "Continue" : "Pause"}
-            </p>
-          </button>
+          <Dialog open={isPauseDialogOpen} onOpenChange={setIsPauseDialogOpen}>
+            <DialogTrigger className="w-full">
+              <button className="xl:w-[152px] w-full h-11 rounded-[10px] bg-[#E4E4E4] flex items-center justify-center gap-1 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed">
+                <PlatePaused />
+                <p className="text-[#192540] text-base font-medium">
+                  {isPaused ? "Continue" : "Pause"}
+                </p>
+              </button>
+            </DialogTrigger>
+            <DialogContent className="w-[860px] px-0!">
+              <DialogHeader>
+                <DialogTitle></DialogTitle>
+                <DialogDescription>
+                  <div>
+                    <div className="flex flex-col items-center justify-center">
+                      <img
+                        src="../../../public/images/pause.png"
+                        alt="pause"
+                        className="w-[245px] h-[245px]"
+                      />
+                      <h2 className="text-[#192540] text-2xl font-semibold mt-4">
+                        {isPaused ? "Continue Your Ad?" : "Pause Your Ad?"}
+                      </h2>
+                      <p className="text-[#717171] text-lg font-medium mt-4">
+                        {isPaused
+                          ? "Are you sure you want to continue this ad?"
+                          : "Are you sure you want to pause this ad? It will no longer appear to buyers until you reactivate it."}
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between gap-6 mt-7 px-8">
+                      <button
+                        onClick={() => setIsPauseDialogOpen(false)}
+                        className="w-full h-14 border border-[#EBAF29] rounded-md text-[#192540] text-lg font-semibold cursor-pointer"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        onClick={() => handleTogglePause(plate.id)}
+                        disabled={isLoading}
+                        className="w-full h-14 bg-[#EBAF29] rounded-md text-[#192540] text-lg font-semibold cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+                      >
+                        {isLoading
+                          ? "Loading..."
+                          : isPaused
+                          ? "Continue Now"
+                          : "Pause Now"}
+                      </button>
+                    </div>
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
+    </div>
     </section>
   );
 };
