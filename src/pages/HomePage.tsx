7f97650832efbcafdc94be7721f36cb3955db084
ahmed <ahmedabozaid.dev@gmail.com>
2025-12-30 13:23:17 +0200

    adds vercel

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,19 @@
-import FilterYourPlates from "../components/home/FilterYourPlates"
-import HomeCategories from "../components/home/HomeCategories"
-import HomeHero from "../components/home/HomeHero"
-import HomeImageSection from "../components/home/HomeImageSection"
-import WhyChooseNumra from "../components/home/WhyChooseNumra"
+import FilterYourPlates from "../components/home/FilterYourPlates";
+import HomeCategories from "../components/home/HomeCategories";
+import HomeHero from "../components/home/HomeHero";
+import HomeImageSection from "../components/home/HomeImageSection";
+import WhyChooseNumra from "../components/home/WhyChooseNumra";
 
 const HomePage = () => {
-    return (
-        <div>
-            <HomeHero />
-            <HomeCategories />
-            <FilterYourPlates />
-            <HomeImageSection />
-            <WhyChooseNumra />
-        </div>
-    )
-}
+  return (
+    <div>
+      <HomeHero />
+      <HomeCategories />
+      <FilterYourPlates />
+      <HomeImageSection />
+      <WhyChooseNumra />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
