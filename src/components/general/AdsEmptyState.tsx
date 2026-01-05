4a30fe23362ddcd0b9d7a7a6c0d4acbe7f79b8b1
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-01-05 12:04:30 +0200

    translation - ads empty state

--- src/components/general/AdsEmptyState.tsx
+++ src/components/general/AdsEmptyState.tsx
@@ -1,11 +1,16 @@
+import { useTranslation } from "react-i18next";
+
 const AdsEmptyState = () => {
+    const { t } = useTranslation("profile");
     return (
         <div className="flex flex-col items-center justify-center">
             <img 
                 src="/images/ads_empty.png"
                 alt="empty"
             />
-            <p className="text-[#192540] text-base font-medium mt-10">Your ads list is empty. Create a new ad to get started.</p>
+            <p className="text-[#192540] text-base font-medium mt-10">
+                {t('ads_list_empty')}
+            </p>
         </div>
     )
 }
