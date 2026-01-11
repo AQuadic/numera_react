071c4e466bedf088f96978a7ddb2eef9d42a6955
moon <ahmedabozaid.dev@gmail.com>
2026-01-11 14:03:00 +0200

    fixes favorites empty state

--- src/components/my_profile/FavEmpty.tsx
+++ src/components/my_profile/FavEmpty.tsx
@@ -1,14 +1,17 @@
+import { useTranslation } from "react-i18next";
+
 const FavEmpty = () => {
-    return (
-        <div className="flex flex-col items-center justify-center">
-            <img 
-                src="../../../public/images/no_fav.png"
-                alt="no fav"
-            />
+  const { t } = useTranslation("profile");
 
-            <p className="text-[#192540] text-xl font-medium mt-8">No favorite plates yet. Start exploring and mark your favorite ones</p>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <img src="/images/no_fav.png" alt="no fav" />
 
-export default FavEmpty
+      <p className="text-[#192540] text-xl font-medium mt-8">
+        {t("no_favorites")}
+      </p>
+    </div>
+  );
+};
+
+export default FavEmpty;
